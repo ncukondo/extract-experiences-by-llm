38a9bf49e40d42c5806d2843267524583c94fd29
ncukondo <ncukondop@gmail.com>
2024-11-13 09:05:43 +0900

    translate comments to English

--- test/test_api.sh
+++ test/test_api.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-# JSONファイルのパス
+# Path to the JSON file
 FILE_PATH="./test/data/sample.json"
 
-# 日付のフォーマット（例：2023_04_19_151200）
+# Date format (e.g., 2023_04_19_151200)
 TIMESTAMP=$(date "+%Y_%m_%d_%H%M_%S")
 OUTPUT_DIR="./test/log"
 STD_LOG="$OUTPUT_DIR/$TIMESTAMP.log"
@@ -15,18 +14,18 @@ TEMP_FILE="$OUTPUT_DIR/temp.json"
 echo $JSON_LOG
 echo $TEXT_LOG
 
-# end point lodal: http://localhost:3000/api
+# end point local: http://localhost:3000/api
 # dummy end point local: http://localhost:3000/api/dummy
 # dummy end point: https://extract-exp-by-llm.vercel.app/api/dummy
 # true end point: https://extract-exp-by-llm.vercel.app/api/
 
-# APIエンドポイント
+# API endpoint
 API_ENDPOINT="http://localhost:3000/api"
 
-# curlを使用してファイルの内容をPOSTする
-curl -o $TEMP_FILE -X POST -H "Content-Type: application/json" -d @$FILE_PATH $API_ENDPOINT  > $STD_LOG  2> $ERROR_LOG
+# Use curl to POST the contents of the file
+curl -o $TEMP_FILE -X POST -H "Content-Type: application/json" -d @$FILE_PATH $API_ENDPOINT > $STD_LOG 2> $ERROR_LOG
 
-# jqを使用して出力ファイルを整形する
+# Use jq to format the output file
 jq '.' $TEMP_FILE > $JSON_LOG
 
 cat $JSON_LOG | bun run ./test/tools/output_converter.ts > $TEXT_LOG
